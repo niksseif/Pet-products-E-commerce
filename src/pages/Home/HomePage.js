4bf3d067edfc4cc27e02f32099a67477a001421c
nikss <nik.safe@gmail.com>
2024-05-07 12:56:12 -0700

    Added assets, product card

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,7 +1,9 @@
 
 import {Hero } from './components/Hero'
+import { FeaturedProducts} from'./components/FeaturedProducts'
   export const HomePage = () => {
   return <main>
     <Hero/>
+    <FeaturedProducts/>
   </main>
 };
