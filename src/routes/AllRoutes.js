c1961b02d59346c18a33c40e91e84c415103e1e8
nikss <nik.safe@gmail.com>
2024-05-28 09:59:34 -0700

    add register,login routes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, ProductList, ProductDetail } from "../pages";
+import { HomePage, ProductList, ProductDetail, Login, Register } from "../pages";
 
 export const AllRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductList />} />
+        <Route path="products" element={<ProductList />} />
         <Route path="products/:id" element={<ProductDetail />} />
+
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </>
   );
