99c68433b4caab31d2aa70392323df3c4970e00a
nikss <nik.safe@gmail.com>
2024-05-27 18:37:36 -0700

    add dropdownLoggedOut to header

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import Logo from "../../assets/logo32x33.png";
 import { useEffect, useState } from "react";
 import { Search } from "../Sections/Search";
+import { DropdownLoggedOut, DropdownLoggedIn } from "../index";
 export function Header() {
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem("darkMode")) || false
   );
   const [showSearch, setShowSearch] = useState(false);
+  const [dropDown, setDropDown] = useState(false)
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -46,13 +48,15 @@ export function Header() {
                 className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search-heart-fill"
               ></span>
               <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-bag-heart"></span>
-              <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-person"></span>
+              <span onClick={() => setDropDown(!dropDown)}className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-person"></span>
+              {dropDown && <DropdownLoggedOut/>}
             </div>
           </div>
         </div>
       </nav>
 
       {showSearch && <Search setShowSearch={setShowSearch} />}
+     
     </header>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { Header } from "../components/Layouts/Header";
 export { Footer } from "../components/Layouts/Footer";
 export { ProductCard } from "./Elements/ProductCard";
 export { ScrollToTop } from "./Other/ScrolltoTop";
+export { DropdownLoggedIn } from './Elements/DropdownLoggedIn'
+export { DropdownLoggedOut } from './Elements/DropdownLoggedOut'
